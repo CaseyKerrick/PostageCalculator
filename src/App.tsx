7772bc20bed587f32763a192f50a60b7cd556399
CaseyKerrick <casey.rainn.kerrick@gmail.com>
2023-08-24 18:11:41 -0500

    Added logo and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { InputAdornment, TextField, List, ListItem, Button } from '@mui/material';
+import fullLogo from './FullLogo_Transparent.png';
 import './App.css';
 
 function App() {
@@ -89,58 +90,63 @@ function App() {
   }, [setPostageCost, setMaxStamps, setStampDenominations]);
 
   return (
-    <div className='mainList'>
-      <div className='dataEntry'>
-        <TextField
-          label="Postage Cost in Cents"
-          className='totalCostCents'
-          value={postageCost}
-          InputProps={{
-            endAdornment: <InputAdornment position="end">¢</InputAdornment>,
-          }}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            const newPostageCost = Number.parseInt(event.target.value) || 0;
-            localStorage.setItem('postageCost', newPostageCost.toString());
-            setPostageCost(newPostageCost);
-          }}
-        />
-        <TextField
-          label="Max # of Stamps"
-          className='maxStamps'
-          value={maxStamps}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            localStorage.setItem('maxStamps', event.target.value);
-            setMaxStamps(Number.parseInt(event.target.value) || 0);
-          }}
-        />
-        <TextField
-          label="Stamp Values Available"
-          className='stampValues'
-          value={stampDenominations}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setStampDenominations(event.target.value || '')}
-          onBlur={() => {
-            const intStampDenoms = stampDenominations.split(',').map(strDenom => Number.parseInt(strDenom.trim()));
-            const strSortedStampDenoms = intStampDenoms.sort(sortInts).toString();
-
-            localStorage.setItem('stampDenominations', strSortedStampDenoms);
-            setStampDenominations(strSortedStampDenoms);
-          }}
-        />
-        <Button
-          variant="contained"
-          onClick={() => generateSolutions()}
-          className="generateSolution"
-        >
-          Generate Solutions
-        </Button>
+    <>
+      <div className="logoHeader">
+        <img src={fullLogo} alt="Snail with an at symbol shell. Happy Mail Postage Calculator" className="logo" />
       </div>
-      <hr className='divider' />
-      <List className='displayPostageSolution'>
-        { solutions.map((sol: number[]) => (
-          <ListItem key={sol.toString()} className='postageSolution'>{drawStamps(sol)}</ListItem>
-        ))}
-      </List>
-    </div>
+      <div className='mainList'>
+        <div className='dataEntry'>
+          <TextField
+            label="Postage Cost in Cents"
+            className='totalCostCents'
+            value={postageCost}
+            InputProps={{
+              endAdornment: <InputAdornment position="end">¢</InputAdornment>,
+            }}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              const newPostageCost = Number.parseInt(event.target.value) || 0;
+              localStorage.setItem('postageCost', newPostageCost.toString());
+              setPostageCost(newPostageCost);
+            }}
+          />
+          <TextField
+            label="Max # of Stamps"
+            className='maxStamps'
+            value={maxStamps}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              localStorage.setItem('maxStamps', event.target.value);
+              setMaxStamps(Number.parseInt(event.target.value) || 0);
+            }}
+          />
+          <TextField
+            label="Stamp Values Available"
+            className='stampValues'
+            value={stampDenominations}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setStampDenominations(event.target.value || '')}
+            onBlur={() => {
+              const intStampDenoms = stampDenominations.split(',').map(strDenom => Number.parseInt(strDenom.trim()));
+              const strSortedStampDenoms = intStampDenoms.sort(sortInts).toString();
+
+              localStorage.setItem('stampDenominations', strSortedStampDenoms);
+              setStampDenominations(strSortedStampDenoms);
+            }}
+          />
+          <Button
+            variant="contained"
+            onClick={() => generateSolutions()}
+            className="generateSolution"
+          >
+            Generate Solutions
+          </Button>
+        </div>
+        <hr className='divider' />
+        <List className='displayPostageSolution'>
+          { solutions.map((sol: number[]) => (
+            <ListItem key={sol.toString()} className='postageSolution'>{drawStamps(sol)}</ListItem>
+          ))}
+        </List>
+      </div>
+    </>
   );
 }
 
